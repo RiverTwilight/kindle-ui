6e0e9c561284b42f212816ee242c0ccf0e981fa3
RiverTwilight <wrj2014@126.com>
2021-07-02 10:25:17 +0000

    feat: statu bar

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -34,15 +34,46 @@ var Airplane = (({
   d: "M186.62 464H160a16 16 0 01-14.57-22.6l64.46-142.25L113.1 297l-35.3 42.77C71.07 348.23 65.7 352 52 352H34.08a17.66 17.66 0 01-14.7-7.06c-2.38-3.21-4.72-8.65-2.44-16.41l19.82-71c.15-.53.33-1.06.53-1.58a.38.38 0 000-.15 14.82 14.82 0 01-.53-1.59l-19.84-71.45c-2.15-7.61.2-12.93 2.56-16.06a16.83 16.83 0 0113.6-6.7H52c10.23 0 20.16 4.59 26 12l34.57 42.05 97.32-1.44-64.44-142A16 16 0 01160 48h26.91a25 25 0 0119.35 9.8l125.05 152 57.77-1.52c4.23-.23 15.95-.31 18.66-.31C463 208 496 225.94 496 256c0 9.46-3.78 27-29.07 38.16-14.93 6.6-34.85 9.94-59.21 9.94-2.68 0-14.37-.08-18.66-.31l-57.76-1.54-125.36 152a25 25 0 01-19.32 9.75z"
 })));
 
-var _templateObject, _templateObject2, _templateObject3, _templateObject4;
-var Navbar = styled__default['default'].nav(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    display: flex;\n    flex-direction: column;\n    border-bottom: 1px solid #000;\n"])));
-var StatuBar = styled__default['default'].div(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n    display: flex;\n    border-bottom: 1px solid #000;\n    padding: 3px 18px;\n    height: 19px;\n"])));
-var ActionBar = styled__default['default'].div(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n    display: flex;\n    height: 50px;\n"])));
-var Action = styled__default['default'].button(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n    border: none;\n    border-right: 1px solid #000;\n    background-color: #fff;\n    width: 50px;\n    transition: background-color 0.5s;\n    &:hover {\n        background-color: #000;\n    }\n"])));
-var index$1 = (function (_ref) {
-  var _ref$deviceName = _ref.deviceName,
-      deviceName = _ref$deviceName === void 0 ? "My Kindle" : _ref$deviceName;
-  return /*#__PURE__*/React__default['default'].createElement(Navbar, null, /*#__PURE__*/React__default['default'].createElement(StatuBar, null, deviceName), /*#__PURE__*/React__default['default'].createElement(ActionBar, null, /*#__PURE__*/React__default['default'].createElement(Action, null, /*#__PURE__*/React__default['default'].createElement(Airplane, null)), /*#__PURE__*/React__default['default'].createElement(Action, null), /*#__PURE__*/React__default['default'].createElement(Action, null)));
+var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5;
+var BORDER_COLOR = "#282424";
+var Navbar = styled__default['default'].nav(_templateObject || (_templateObject = _taggedTemplateLiteral(["\n    display: flex;\n    flex-direction: column;\n    border-bottom: 1px solid ", ";\n"])), BORDER_COLOR);
+var Action = styled__default['default'].button(_templateObject2 || (_templateObject2 = _taggedTemplateLiteral(["\n    border: none;\n    background-color: #fff;\n    width: 50px;\n    transition: background-color 0.5s;\n    svg{\n        width: 23px\n    }\n    &:hover {\n        background-color: #000;\n        svg {\n            stroke: #fff;\n        }\n    }\n"])));
+var StyledAirplane = styled__default['default'](Airplane)(_templateObject3 || (_templateObject3 = _taggedTemplateLiteral(["\n    width: 18px;\n"])));
+
+var StatuBar = function StatuBar(_ref) {
+  var className = _ref.className,
+      children = _ref.children;
+  return /*#__PURE__*/React__default['default'].createElement("div", {
+    className: className
+  }, children);
+};
+
+var ActionBar = function ActionBar(_ref2) {
+  var className = _ref2.className,
+      children = _ref2.children;
+  return /*#__PURE__*/React__default['default'].createElement("div", {
+    className: className
+  }, children);
+};
+
+var StyledStatuBar = styled__default['default'](StatuBar)(_templateObject4 || (_templateObject4 = _taggedTemplateLiteral(["\n    display: flex;\n    justify-content: space-between;\n    border-bottom: 1px solid #000;\n    padding: 3px 18px;\n    height: 19px;\n    .statu-item {\n        margin-left: 10px;\n    };\n    .time {\n        transform: scaleY(-50%)\n    }\n"])));
+var StyledActionBar = styled__default['default'](ActionBar)(_templateObject5 || (_templateObject5 = _taggedTemplateLiteral(["\n    display: flex;\n    height: 50px;\n"])));
+/**
+ * Navbar组件
+ * @author rivertwilight
+ * @param {string} deviceName 设备名称
+ */
+
+var index$1 = (function (_ref3) {
+  var _ref3$deviceName = _ref3.deviceName,
+      deviceName = _ref3$deviceName === void 0 ? "My Kindle" : _ref3$deviceName;
+  return /*#__PURE__*/React__default['default'].createElement(Navbar, null, /*#__PURE__*/React__default['default'].createElement(StyledStatuBar, null, /*#__PURE__*/React__default['default'].createElement("div", {
+    className: "statu-item"
+  }, deviceName), /*#__PURE__*/React__default['default'].createElement("div", {
+    className: "statu-item"
+  }, /*#__PURE__*/React__default['default'].createElement(StyledAirplane, null), /*#__PURE__*/React__default['default'].createElement("span", {
+    className: "time"
+  }, "15:00 AM"))), /*#__PURE__*/React__default['default'].createElement(StyledActionBar, null, /*#__PURE__*/React__default['default'].createElement(Action, null, /*#__PURE__*/React__default['default'].createElement(Airplane, null), "Home"), /*#__PURE__*/React__default['default'].createElement(Action, null, /*#__PURE__*/React__default['default'].createElement(Airplane, null), "Home"), /*#__PURE__*/React__default['default'].createElement(Action, null, /*#__PURE__*/React__default['default'].createElement(Airplane, null), "Home")));
 });
 
 var index = (function () {
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,47 +1,89 @@
 import styled from "styled-components";
 import React from "react";
 import Airplane from "../../icons/airplane.svg";
+import BatteryFull from "../../icons/battery-full.svg";
+
+const BORDER_COLOR = "#282424"
 
 const Navbar = styled.nav`
     display: flex;
     flex-direction: column;
-    border-bottom: 1px solid #000;
-`;
-
-const StatuBar = styled.div`
-    display: flex;
-    border-bottom: 1px solid #000;
-    padding: 3px 18px;
-    height: 19px;
-`;
-
-const ActionBar = styled.div`
-    display: flex;
-    height: 50px;
+    border-bottom: 1px solid ${BORDER_COLOR};
 `;
 
 const Action = styled.button`
     border: none;
-    border-right: 1px solid #000;
     background-color: #fff;
     width: 50px;
     transition: background-color 0.5s;
+    svg{
+        width: 23px
+    }
     &:hover {
         background-color: #000;
+        svg {
+            stroke: #fff;
+        }
     }
 `;
 
+const StyledAirplane = styled(Airplane)`
+    width: 18px;
+`;
+
+const StatuBar = ({ className, children }) => (
+    <div className={className}>{children}</div>
+);
+
+const ActionBar = ({ className, children }) => (
+    <div className={className}>{children}</div>
+);
+
+const StyledStatuBar = styled(StatuBar)`
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #000;
+    padding: 3px 18px;
+    height: 19px;
+    .statu-item {
+        margin-left: 10px;
+    };
+    .time {
+        transform: scaleY(-50%)
+    }
+`;
+
+const StyledActionBar = styled(ActionBar)`
+    display: flex;
+    height: 50px;
+`;
+
+/**
+ * Navbar组件
+ * @author rivertwilight
+ * @param {string} deviceName 设备名称
+ */
 export default ({ deviceName = "My Kindle" }) => {
     return (
         <Navbar>
-            <StatuBar>{deviceName}</StatuBar>
-            <ActionBar>
+            <StyledStatuBar>
+                <div className="statu-item">{deviceName}</div>
+                <div className="statu-item">
+                    <StyledAirplane />
+                    <span className="time">15:00 AM</span>
+                </div>
+            </StyledStatuBar>
+            <StyledActionBar>
                 <Action>
-                    <Airplane />
+                    <Airplane />Home
                 </Action>
-                <Action></Action>
-                <Action></Action>
-            </ActionBar>
+                <Action>
+                    <Airplane />Home
+                </Action>
+                <Action>
+                    <Airplane />Home
+                </Action>
+            </StyledActionBar>
         </Navbar>
     );
 };
